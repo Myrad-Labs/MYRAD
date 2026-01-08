4bc7dde45ab8e819f448204d19a2b5348004abb6
Sayangenri <sayangenri1@gmail.com>
2026-01-08 20:06:20 +0530

    delete unuse images

--- src/pages/PrivacyPolicyPage.tsx
+++ src/pages/PrivacyPolicyPage.tsx
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom';
 import { Shield } from 'lucide-react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
--- src/pages/TermsOfServicePage.tsx
+++ src/pages/TermsOfServicePage.tsx
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom';
 import { FileText } from 'lucide-react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
